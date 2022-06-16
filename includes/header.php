e452fcf15e1e9094ad9a93136cbbe68253685c78
Jonathan <alexiscdmovil@live.com>
2022-06-16 12:17:16 -0500

    Descarga de bootstrap

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP PARA PICO Y PLACA</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <a href="index.php" class=navbar-brand>PICO Y PLACA</a>
+    </div>  
+</nav>  
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include("servicios.php")?>
+<?php include("includes/header.php") ?>
+
+<div class="container p-4">
+</div>
+
+<?php include("includes/footer.php") ?>
