5fe3f20da5db88c594aafde08c5719c33afa6434
Jonathan <alexiscdmovil@live.com>
2022-06-16 17:05:35 -0500

    Llenado de datos
    se lleno los datos en las tablas y ahora solo falta las funciones de editar y eliminar

--- index.php
+++ index.php
@@ -1,34 +1,93 @@
-<?php include("servicios.php")?>
+<?php include("servicios.php") ?>
 <?php include("includes/header.php") ?>
 
 <main class="container p-4">
     <div class="row">
         <div class="col-md-4">
             <div class="card card-body">
-                <form action="">
+                <form name="formularioDatos" action="index.php" method="post">
                     <div class="form-group">
                         <h5>INGRESE EL ULTIMO NUMERO DE SU PLACA</h5>
                         <br>
-                        <input type="text" name="title" class="form-control" placeholder="Task Tittle" autofocus>
+                        <input type="text" name="title" class="form-control" placeholder="Ingrese Ultimo Numero de la Placa" autofocus>
                         <br>
                     </div>
-                    <input type="submit" class="btn btn-success btn-block" value="Save Task" name="save_task">
-                        <br>
+                    <input type="submit" class="btn btn-success btn-block" value="Consultar" name="save_task">
+                    <br>
                 </form>
             </div>
         </div>
-        <div class="col-md-8"> 
+        <div class="col-md-4">
+            <?php $numero = $_POST['title']; ?>
             <table class="table table-bordered">
                 <thead>
                     <tr>
                         <th>Dias que PUEDE Circular</th>
-                        <th>Dias que NO PUEDE Circular</th>
+                        <th>Horarios</th>
                     </tr>
                 </thead>
-
+                <tbody>
+                    <?php echo diasTransitables($numero); ?>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-md-4">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Dias que NO PUEDE Circular</th>
+                        <th>Horarios</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $numero = $_POST['title']; ?>
+                    <tr>
+                        <td><?php echo diasNoTransitables($numero); ?></td>
+                        <td><?php echo horarioNoCircula($numero); ?></td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card card-body">
+                <form name="formularioDatos" action="index.php" method="post">
+                    <div class="form-group">
+                        <h5>Editar Horaios</h5>
+                        <br>
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>id</th>
+                                    <th>Dia</th>
+                                    <th>Horario</th>
+                                    <th>Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach (editarHorario() as $key => $row) {
+                                    $id[$key] = $row['id'];
+                                    $dia[$key] = $row['Dia'];
+                                    $horario[$key] = $row['Horario']; ?>
+                                    <tr>
+                                        <td><?php echo $id[$key]; ?> </td>
+                                        <td><?php echo $dia[$key]; ?> </td>
+                                        <td><?php echo $horario[$key]; ?> </td>
+                                        <td> <a href="edit.php?id=<?php echo $numero ?>" class="btn btn-secondary"><i class="fas fa-marker"></i></a>
+                                            <a href="index.php?id=<?php echo $id ?>" class="btn btn-danger"><i class="fas fa-trash-alt"> <?php echo eliminar($id) ?> </i></a>
+                                        </td>
+                                    </tr>
+                                <?php  } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
 </main>
 
 <?php include("includes/footer.php") ?>
--- servicios.php
+++ servicios.php
@@ -1,4 +1,131 @@
 <?php
 
+function diasTransitables($numPlaca)
+{
+    $resultado = null;
+    if ($numPlaca == 1 || $numPlaca == 2) {
+        $resultado =  "
+        <tr><td>MARTES</td><td>" . horarioCircula() . "</td></tr>
+        <tr><td>MIERCOLES</td><td>" . horarioCircula() . "</td></tr>
+        <tr><td>JUEVES</td><td>" . horarioCircula() . "</td></tr>
+        <tr><td>VIERNES</td><td>" . horarioCircula() . "</td></tr>
+        <tr><td>SABADO</td><td>" . horarioCircula() . "</td></tr>
+        <tr><td>DOMINGO</td><td>" . horarioCircula() . "</td></tr>";
+    } else if ($numPlaca == 3 || $numPlaca == 4) {
+        $resultado = "
+        <tr><td>LUNES</td><td>" . horarioCircula() . "</td></tr>
+        <tr><td>MIERCOLES</td><td>" . horarioCircula() . "</td></tr>
+        <tr><td>JUEVES</td><td>" . horarioCircula() . "</td></tr>
+        <tr><td>VIERNES</td><td>" . horarioCircula() . "</td></tr>
+        <tr><td>SABADO</td><td>" . horarioCircula() . "</td></tr>
+        <tr><td>DOMINGO</td><td>" . horarioCircula() . "</td></tr>";
+    } else if ($numPlaca == 5 || $numPlaca == 6) {
+        $resultado =  "
+            <tr><td>LUNES</td><td>" . horarioCircula() . "</td></tr>
+            <tr><td>MARTES</td><td>" . horarioCircula() . "</td></tr>
+            <tr><td>JUEVES</td><td>" . horarioCircula() . "</td></tr>
+            <tr><td>VIERNES</td><td>" . horarioCircula() . "</td></tr>
+            <tr><td>SABADO</td><td>" . horarioCircula() . "</td></tr>
+            <tr><td>DOMINGO</td><td>" . horarioCircula() . "</td></tr>";
+    } else if ($numPlaca == 7 || $numPlaca == 8) {
+        $resultado =  "
+                <tr><td>LUNES</td><td>" . horarioCircula() . "</td></tr>
+                <tr><td>MARTES</td><td>" . horarioCircula() . "</td></tr>
+                <tr><td>MIERCOLES</td><td>" . horarioCircula() . "</td></tr>
+                <tr><td>VIERNES</td><td>" . horarioCircula() . "</td></tr>
+                <tr><td>SABADO</td><td>" . horarioCircula() . "</td></tr>
+                <tr><td>DOMINGO</td><td>" . horarioCircula() . "</td></tr>";
+    } else if ($numPlaca == 9 || $numPlaca == 0) {
+        $resultado =  "
+                    <tr><td>LUNES</td><td>" . horarioCircula() . "</td></tr>
+                    <tr><td>MARTES</td><td>" . horarioCircula() . "</td></tr>
+                    <tr><td>MIERCOLES</td><td>" . horarioCircula() . "</td></tr>
+                    <tr><td>JUEVES</td><td>" . horarioCircula() . "</td></tr>
+                    <tr><td>SABADO</td><td>" . horarioCircula() . "</td></tr><tr>
+                    <td>DOMINGO</td><td>" . horarioCircula() . "</td></tr>";
+    } else {
+        $resultado = "";
+    }
+    return $resultado;
+}
 
-?>
+function diasNoTransitables($numPlaca)
+{
+    $resultado = null;
+    if ($numPlaca == 1 || $numPlaca == 2) {
+        $resultado = "NO CIRCULA LUNES";
+    } else if ($numPlaca == 3 || $numPlaca == 4) {
+        $resultado = "NO CIRCULA MARTES";
+    } else if ($numPlaca == 5 || $numPlaca == 6) {
+        $resultado = "NO CIRCULA MIERCOLES";
+    } else if ($numPlaca == 7 || $numPlaca == 8) {
+        $resultado = "NO CIRCULA JUEVES";
+    } else if ($numPlaca == 9 || $numPlaca == 0) {
+        $resultado = "NO CIRCULA VIERNES";
+    } else {
+        $resultado = "";
+    }
+    return $resultado;
+}
+
+function horarioNoCircula($numPlaca)
+{
+    if ($numPlaca == 1 || $numPlaca == 2 || $numPlaca == 3 || $numPlaca == 4 || $numPlaca == 5 || $numPlaca == 6 || $numPlaca == 7 || $numPlaca == 8 || $numPlaca == 9 || $numPlaca == 0) {
+        $resultado = "05:00-10:00 a.m. y 16:00-21:00 p.m.";
+    } else {
+        $resultado = "";
+    }
+
+    return $resultado;
+}
+
+function horarioCircula()
+{
+    $resultado = "Libre Circulacion";
+
+    return $resultado;
+}
+
+function editarHorario()
+{
+    $horarios = [
+        [
+            'id' => 1,
+            'Dia' => 'Lunes',
+            'Horario' => '05:00-10:00 a.m. y 16:00-21:00 p.m.'
+        ],
+        [
+            'id' => 2,
+            'Dia' => 'Martes',
+            'Horario' => '05:00-10:00 a.m. y 16:00-21:00 p.m.'
+        ],
+        [
+            'id' => 3,
+            'Dia' => 'Miercoles',
+            'Horario' => '05:00-10:00 a.m. y 16:00-21:00 p.m.'
+        ],
+        [
+            'id' => 4,
+            'Dia' => 'Jueves',
+            'Horario' => '05:00-10:00 a.m. y 16:00-21:00 p.m.'
+        ],
+        [
+            'id' => 5,
+            'Dia' => 'Viernes',
+            'Horario' => '05:00-10:00 a.m. y 16:00-21:00 p.m.'
+        ]
+    ];
+ return $horarios;
+}
+
+function eliminar($ids){
+    $datos=editarHorario();
+
+    foreach ($datos as $key => $row) {
+        $id[$key] = $row['id'];
+        if($ids==$id[$key]){
+            unset($datos[$id[$key]]);
+        }
+    }
+
+}
